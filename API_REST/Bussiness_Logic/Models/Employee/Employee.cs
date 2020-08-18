ea4f2030dee05874e28b16a6cda4829a3bcb57d4
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2020-08-18 14:32:57 -0500

    Back-End first Update

---
+++ API_REST/Bussiness_Logic/Models/Employee/Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bussiness_Logic.Models.Employee
+{
+    /// <summary>
+    /// Class Employee with 4 properites or fieds, like:
+    /// Id_Employee: Int;
+    /// NumberIdentification: String;
+    /// FirstName: String;
+    /// LastName: String;
+    /// and finally two fields: 1) Id_EmpType: Int. 2) Employee_Type: Employee_Type. This fields, are functional for the create a relationship whit the Table EmployeeType
+    /// </summary>
+
+    [Serializable]
+    public class Employee
+    {
+        [Key]
+        public int Id_Employee { get; set; }
+
+        [StringLength(50, ErrorMessage = "No puede exceder 50 caracteres")]
+        [Display(Name = "Nombres", Description = "Nombres", Prompt = "Nombres...")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "* Apellidos Requeridos...")]
+        [StringLength(50, ErrorMessage = "No puede exceder 50 caracteres")]
+        [Display(Name = "Apellidos", Description = "Apellidos", Prompt = "Apellidos...")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "* Número de Identifiación Requerido...")]
+        [StringLength(10, ErrorMessage = "No puede exceder 10 digitos")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "* No. Identificación Incorrecto...")]
+        [Display(Name = "Número Documento", Description = "Número de Documento", Prompt = "Número de Identificación...")]
+        public string NumberDocument { get; set; }
+
+        [Required]
+        public int Id_EmpType { get; set; }
+
+        public Employee_Type Employee_Type { get; set; }
+
+    }
+}
---
+++ API_REST/Bussiness_Logic/Models/Employee/Employee_Type.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Bussiness_Logic.Models.Employee
+{
+    /// <summary>
+    /// Class Employee_Type with 2 properites like:
+    /// Id_EmployeeType: Int;
+    /// EmpDescription: String;
+    /// And Icolection Employee for establish the relationship whit this table...
+    /// </summary>
+
+    [Serializable]
+    public class Employee_Type
+    {
+        [Key]
+        public int Id_EmployeeType { get; set; }
+
+        [Required(ErrorMessage = "* Tipo Empleado Requeridos...")]
+        [StringLength(50, ErrorMessage = "No puede exceder 50 caracteres")]
+        [Display(Name = "Tipo Empleado", Description = "Tipo Empleado", Prompt = "Tipo Empleado...")]
+        public string EmpDescription { get; set; }
+
+        public ICollection<Employee> Employees { get; set; }
+
+    }
+}
