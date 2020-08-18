2ec3b626087ff719d42363c490701c9d595a37d5
jalejandrosandoval5 <61461851+jalejandrosandoval5@users.noreply.github.com>
2020-08-18 15:56:06 -0500

    Generation Components,Models and Services

--- Front-Angular/src/app/app.module.ts
+++ Front-Angular/src/app/app.module.ts
@@ -3,12 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+//Employee
 import { EmployeeComponent } from './Components/Employee/Employee/employee.component';
 import { EmployeeCreateComponent } from './Components/Employee/employee-create/employee-create.component';
 import { EmployeeEditComponent } from './Components/Employee/employee-edit/employee-edit.component';
-import { EmployeeTypeComponent } from './Components/EmployeeType/EmployeeType/employee-type/employee-type.component';
-import { EmployeeTypeCreateComponent } from './Components/EmployeeType/EmployeeType/employee-type-create/employee-type-create.component';
-import { EmployeeTypeEditComponent } from './Components/EmployeeType/EmployeeType/employee-type-edit/employee-type-edit.component';
+
+//EmployeeType
+import { EmployeeTypeComponent } from './Components/EmployeeType/employee-type/employee-type.component';
+import { EmployeeTypeCreateComponent } from './Components/EmployeeType/employee-type-create/employee-type-create.component';
+import { EmployeeTypeEditComponent } from './Components/EmployeeType/employee-type-edit/employee-type-edit.component';
 
 @NgModule({
   declarations: [
